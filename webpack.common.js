4b0c4a6836760e7db4523db6c59e8c46a1507c73
ahamed07sajeeb <sajeeb07ahamed@gmail.com>
2019-01-27 23:52:05 +0600

    initial commit of gallery package with webpack

---
+++ webpack.common.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Gallery Management',
+            template: './index.html',
+            inject: 'head',
+            favicon: './favicon.ico'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'SP',
+        libraryTarget: 'umd'
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_module/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
